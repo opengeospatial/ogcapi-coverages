d1a8dee0bc2cf7772fd6dc0a8b24c2c135bd43d2
Stephan Meißl <stephan.meissl@eox.at>
2020-08-12 16:13:59 +0200

    fix wording in charter

--- charter/PDF_Gen.bat
+++ charter/PDF_Gen.bat
@@ -1,1 +1,1 @@
-asciidoctor -r asciidoctor-pdf --trace -b pdf -o ..\OGC-Coverages-SWG-Charter.pdf OGC-Coverages-SWG-Charter.adoc
+asciidoctor -r asciidoctor-pdf --trace -b pdf -o ../OGC-Coverages-SWG-Charter.pdf OGC-Coverages-SWG-Charter.adoc
