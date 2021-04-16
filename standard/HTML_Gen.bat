84ddbcc8a543fa56b266457344de6f42ce8be916
Chuck Heazel <cheazel@wiscenterprises.com>
2021-04-16 13:00:49 -0400

    Added PDF and HTML versions

--- standard/HTML_Gen.bat
+++ standard/HTML_Gen.bat
@@ -1,1 +1,1 @@
-asciidoctor --trace -o ./19-087.html ./19-087.adoc
+asciidoctor --trace -o ./19-087-U.html ./19-087.adoc
--- standard/PDF_Gen.bat
+++ standard/PDF_Gen.bat
@@ -1,1 +1,1 @@
-asciidoctor -r asciidoctor-pdf --trace -b pdf -o ./19-087.pdf ./19-087.adoc
+asciidoctor -r asciidoctor-pdf --trace -b pdf -o ./19-087-U.pdf ./19-087.adoc
